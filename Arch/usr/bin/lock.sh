a339baf7ba61c47e00afb7207a344e30add4649b
SKRUZO <SKRUZO@gmail.com>
2017-11-13 18:35:33 -0800

    Current setup
    Files as of 11/13/17

---
+++ Arch/usr/bin/lock.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+[[ -f $HOME/Pictures/Icons/lock.png ]] && convert /tmp/screen.png $HOME/Pictures/Icons/lock.png -gravity center -composite -matte /tmp/screen.png
+i3lock -e -i /tmp/screen.png
